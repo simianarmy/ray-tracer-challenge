2af75d38fc76a49135b08ee275d36dd26dafc9d9
Marc Mauger <simianarmy@gmail.com>
2019-06-08 15:28:40 -0700

    cube tests

--- src/lib/math.js
+++ src/lib/math.js
@@ -14,4 +14,25 @@ export const perlin = (x, y, z) => {
   return perlin.Noise.perlin3(x, y, z);
 }
 
+/**
+ * generalized ray-plane intersection alg. helper
+ * assuming planes offset -1, 1 from origin
+ * @param {Number} origin
+ * @param {Number} direction
+ * @returns [Number, Number] tmin, tmax
+ */
+export const checkAxis = (origin, direction) => {
+  const tminNumerator = -1 - origin;
+  const tmaxNumerator = 1 - origin;
+  let tmin, tmax;
 
+  if (Math.abs(direction) >= EPSILON) {
+    tmin = tminNumerator / direction;
+    tmax = tmaxNumerator / direction;
+  } else {
+    tmin = tminNumerator * Number.POSITIVE_INFINITY;
+    tmax = tmaxNumerator * Number.POSITIVE_INFINITY;
+  }
+
+  return (tmin > tmax) ? [tmax, tmin] : [tmin, tmax];
+}
