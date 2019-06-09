ee501c2b41d3cbd138a7da5af044b4522c1553b5
Marc Mauger <simianarmy@gmail.com>
2019-06-09 16:25:24 -0700

    took a long time burt got an interesting cube scene

--- src/project13.js
+++ src/project13.js
@@ -31,40 +31,57 @@ import "./App.css";
 
 // Control resolution = render time
 const ProjectTitle = "Project 13";
-const HSIZE = 100;
-const VSIZE = 50;
+const HSIZE = 200;
+const VSIZE = 100;
 const RESOLUTION = 2;
 const CANVAS_SCALE = 4;
 
-function Project6() {
-    const fpattern = new SolidPattern(Color(0.2, 0.2, 0.1));
-    fpattern.setTransform(multiply(rotationZ(0.5), scaling(0.9, 0.9, 0.9)));
+function Project13() {
+  const fpattern = new SolidPattern(Color(0.4, 0.4, 0.8));
+  //fpattern.setTransform(multiply(rotationZ(0.5), scaling(0.9, 0.9, 0.9)));
 
-    const cube = new Cube();
-    cube.setTransform(multiply(multiply(translation(-0.5, 0, 1.5), rotationY(0.5)), rotationX(0.2)));
-    cube.material.color = Color(0.2, .4, 0.5);
-    cube.material.ambient = 0.1;
-    cube.material.diffuse = 0.2;
-    cube.material.specular = 1.0;
-    cube.material.shininess = 300;
-    cube.material.transparency = 0.7;
-    cube.material.reflective = 0.9;
-    const mpattern = new Checkers(
-      new SolidPattern(Color(0, 1, 0.2)),
-      new SolidPattern(Color(0, 0.1, 1))
-      );
-    //cube.pattern = mpattern;
+  const room = new Cube();
+  room.material.ambient = 0.2;
+  room.material.reflective = 0.7;
+  room.material.pattern = fpattern;
+  //room.setTransform(multiply(multiply(rotationY(-0.2), scaling(10, 10, 10)), translation(0, 0, -2)));
+  room.setTransform(multiply(multiply(translation(0, 4, -2), rotationY(1)), scaling(8, 8, 8)));
 
-    const world = World();
-    world.objects = [cube];
-    world.lightSource = PointLight(point(-10, 10, -10), Color(1, 1, 1));
+  const cube = new Cube();
+  cube.setTransform(multiply(multiply(translation(-1.5, 0.3, 2.5), rotationY(-0.5)), rotationX(0.2)));
+  cube.material.ambient = 0.8;
+  cube.material.diffuse = 0.2;
+  cube.material.specular = 1.0;
+  cube.material.shininess = 300;
+  const mpattern = new Checkers(
+    new SolidPattern(Color.White),
+    new SolidPattern(Color.Black)
+  );
+  mpattern.setTransform(scaling(0.2, 0.2, 0.2));
+  cube.material.pattern = mpattern;
 
-    const camera = Camera(HSIZE * RESOLUTION, VSIZE * RESOLUTION, Math.PI / 3);
-    camera.transform = viewTransform(
-      point(0, 1.5, -5),
-      point(0, 1, 0),
-      vector(0, 1, 0)
-    );
+  const cube2 = new Cube();
+  cube2.setTransform(multiply(multiply(translation(2, 0.4, 2), rotationY(-0.5)), scaling(0.5, 0.5, 0.5)));
+  cube2.material.color = Color(0, 0, 0.8);
+
+  const cube3 = new Cube();
+  cube3.setTransform(multiply(multiply(multiply(multiply(translation(.5, .8, 2), rotationX(-0.5)), rotationY(Math.PI/4)), rotationZ(0.6)), scaling(3.5, 0.2, 0.2)));
+  const rpattern = new RadialGradient(
+    new SolidPattern(Color.White),
+    new SolidPattern(Color(0, 0.2, 0.5))
+  );
+  cube3.material.pattern = rpattern;
+
+  const world = World();
+  world.objects = [room, cube, cube2, cube3];
+  world.lightSource = PointLight(point(-6, 6, -6), Color(1, 1, 1));
+
+  const camera = Camera(HSIZE * RESOLUTION, VSIZE * RESOLUTION, Math.PI / 3);
+  camera.transform = viewTransform(
+    point(0, 1.5, -7),
+    point(0, 0, 1),
+    vector(0, 1, 0)
+  );
 
   return (
     <div className="App project">
@@ -76,4 +93,4 @@ function Project6() {
   );
 }
 
-export default Project6;
+export default Project13;
