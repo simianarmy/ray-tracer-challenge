c2f91879d95c40ceb9ece3547a5e02eed68bf09b
Marc Mauger <simianarmy@gmail.com>
2019-06-09 20:51:14 -0700

    cylinder end caps

--- src/lib/cylinder.js
+++ src/lib/cylinder.js
@@ -3,13 +3,30 @@ import { EPSILON } from "./math";
 import { Intersection } from "./intersection";
 import { vector } from "./tuple";
 
+// intersection helpers
+// checks to see if the intersection at `t` is within a radius
+// of 1 (the radius of your cylinders) from the y axis.
+const checkCap = (ray, t) => {
+  const x = ray.origin.x + t * ray.direction.x;
+  const z = ray.origin.z + t * ray.direction.z;
+
+  return (x * x + z * z) <= 1;
+}
+
 class Cylinder extends Shape {
+  constructor(props) {
+    super(props);
+    this.minimum = Number.NEGATIVE_INFINITY;
+    this.maximum = Number.POSITIVE_INFINITY;
+    this.closed = false;
+  }
+
   localIntersect(ray) {
     const a = Math.pow(ray.direction.x, 2) + Math.pow(ray.direction.z, 2);
 
     // if ray is parallel to the y axis
     if (Math.abs(a) < EPSILON) {
-      return [];
+      return this.intersectCaps(ray, []);
     }
 
     const b = (2 * ray.origin.x * ray.direction.x) +
@@ -21,14 +38,62 @@ class Cylinder extends Shape {
       return [];
     }
 
-    const t0 = (-b - Math.sqrt(disc)) / (2 * a);
-    const t1 = (-b + Math.sqrt(disc)) / (2 * a);
+    let t0 = (-b - Math.sqrt(disc)) / (2 * a);
+    let t1 = (-b + Math.sqrt(disc)) / (2 * a);
 
-    return [Intersection(t0, this), Intersection(t1, this)];
+    if (t0 > t1) {
+      const temp = t0;
+      t0 = t1;
+      t1 = temp;
+    }
+
+    let xs = [t0, t1].reduce((acc, curr) => {
+      const y = ray.origin.y + curr * ray.direction.y;
+
+      if (this.minimum < y && y < this.maximum) {
+        acc.push(Intersection(curr, this));
+      }
+      return acc;
+    }, []);
+
+    return this.intersectCaps(ray, xs);
   }
 
   localNormalAt(p) {
+    const dist = p.x * p.x + p.z * p.z;
+
+    if (dist < 1 && p.y >= this.maximum - EPSILON) {
+      return vector(0, 1, 0);
+    } else if (dist < 1 && p.y <= this.minimum + EPSILON) {
+      return vector(0, -1, 0);
+    }
+
     return vector(p.x, 0, p.z);
+  }
+
+  /**
+   * @modifies xs
+   */
+  intersectCaps(ray, xs) {
+    if (!this.closed || (Math.abs(ray.direction.y) < EPSILON)) {
+      return xs;
+    }
+
+    // check for intersection with lower cap
+    let t = (this.minimum - ray.origin.y) / ray.direction.y;
+
+    if (checkCap(ray, t)) {
+      xs.push(Intersection(t, this));
+    }
+
+    // check for intersection with upper cap
+    t = (this.maximum - ray.origin.y) / ray.direction.y;
+
+    if (checkCap(ray, t)) {
+      xs.push(Intersection(t, this));
+    }
+
+    return xs;
   }
 }
 
--- src/lib/cylinder.test.js
+++ src/lib/cylinder.test.js
@@ -10,6 +10,10 @@ describe("Cylinder", () => {
     shape = new Cylinder();
   });
 
+  it("should be open by default", () => {
+    expect(shape.closed).not.toBeTruthy();
+  });
+
   describe("ray intersection", () => {
     it("should detect non-intersection", () => {
       const testCases = [
@@ -39,10 +43,50 @@ describe("Cylinder", () => {
         expect(floatIsEqual(xs[1].t, t.t1)).toBeTruthy();
       });
     });
+
+    describe("on constrained cylinder", () => {
+      beforeEach(() => {
+        shape.minimum = 1;
+        shape.maximum = 2;
+      });
+
+      it("should detect intersections", () => {
+        const testCases = [
+          {origin: point(0, 1.5, 0), direction: vector(0.1, 1, 0), count: 0},
+          {origin: point(0, 3, -5), direction: vector(0, 0, 1), count: 0},
+          {origin: point(0, 0, -5), direction: vector(0, 0, 1), count: 0},
+          {origin: point(0, 2, -5), direction: vector(0, 0, 1), count: 0},
+          {origin: point(0, 1, -5), direction: vector(0, 0, 1), count: 0},
+          {origin: point(0, 1.5, -2), direction: vector(0, 0, 1), count: 2},
+        ];
+        testCases.forEach(t => {
+          const ray = Ray(t.origin, normalize(t.direction));
+          const xs = shape.localIntersect(ray);
+          expect(xs.length).toBe(t.count);
+        });
+      });
+
+      it("should detect intersection with caps of closed cylinder", () => {
+        shape.closed = true;
+
+        const testCases = [
+          {origin: point(0, 3, 0), direction: vector(0, -1, 0), count: 2},
+          {origin: point(0, 3, -2), direction: vector(0, -1, 2), count: 2},
+          {origin: point(0, 4, -2), direction: vector(0, -1, 1), count: 2},
+          {origin: point(0, 0, -2), direction: vector(0, 1, 2), count: 2},
+          {origin: point(0, -1, -2), direction: vector(0, 1, 1), count: 2},
+        ];
+        testCases.forEach(t => {
+          const ray = Ray(t.origin, normalize(t.direction));
+          const xs = shape.localIntersect(ray);
+          expect(xs.length).toBe(t.count);
+        });
+      });
+    });
   });
 
   describe("normal", () => {
-    it("should be computed", () => {
+    it("should be computed for points on surface", () => {
       const testCases = [
         {point: point(1, 0, 0), normal: vector(1, 0, 0)},
         {point: point(0, 5, -1), normal: vector(0, 0, -1)},
@@ -52,6 +96,31 @@ describe("Cylinder", () => {
       testCases.forEach(t => {
         expect(shape.localNormalAt(t.point)).toEqualVector(t.normal);
       });
+    });
+
+    it("should be computed for points on closed cyclinder caps", () => {
+      shape.closed = true;
+      shape.minimum = 1;
+      shape.maximum = 2;
+
+      const testCases = [
+        {point: point(0, 1, 0), normal: vector(0, -1, 0)},
+        {point: point(0.5, 1, 0), normal: vector(0, -1, 0)},
+        {point: point(0, 1, 0.5), normal: vector(0, -1, 0)},
+        {point: point(0, 2, 0), normal: vector(0, 1, 0)},
+        {point: point(0.5, 2, 0), normal: vector(0, 1, 0)},
+        {point: point(0, 2, 0.5), normal: vector(0, 1, 0)},
+      ];
+      testCases.forEach(t => {
+        expect(shape.localNormalAt(t.point)).toEqualVector(t.normal);
+      });
+    });
+  });
+
+  describe("bounds", () => {
+    it("should have infinite minimum and maximum bounds", () => {
+      expect(shape.minimum).toBe(Number.NEGATIVE_INFINITY);
+      expect(shape.maximum).toBe(Number.POSITIVE_INFINITY);
     });
   });
 });
