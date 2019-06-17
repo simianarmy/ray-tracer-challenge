e33bc7b2b47776a6780687192611fbf850b4da86
Marc Mauger <simianarmy@gmail.com>
2019-06-17 16:11:27 -0700

    implemented cone shape

---
+++ src/lib/cone.js
@@ -0,0 +1,116 @@
+import { Shape } from "./shape";
+import { EPSILON } from "./math";
+import { Intersection } from "./intersection";
+import { vector } from "./tuple";
+
+// intersection helpers
+// checks to see if the intersection at `t` is within the radius of the cone
+const checkCap = (ray, t, y) => {
+  const x = ray.origin.x + t * ray.direction.x;
+  const z = ray.origin.z + t * ray.direction.z;
+
+  return x * x + z * z <= Math.abs(y);
+};
+
+class Cone extends Shape {
+  constructor(props) {
+    super(props);
+    this.minimum = Number.NEGATIVE_INFINITY;
+    this.maximum = Number.POSITIVE_INFINITY;
+    this.closed = false;
+  }
+
+  localIntersect(ray) {
+    const a =
+      Math.pow(ray.direction.x, 2) -
+      Math.pow(ray.direction.y, 2) +
+      Math.pow(ray.direction.z, 2);
+    const b =
+      2 * ray.origin.x * ray.direction.x -
+      2 * ray.origin.y * ray.direction.y +
+      2 * ray.origin.z * ray.direction.z;
+    const c =
+      Math.pow(ray.origin.x, 2) -
+      Math.pow(ray.origin.y, 2) +
+      Math.pow(ray.origin.z, 2);
+
+    // if ray is parallel to the y axis
+    if (Math.abs(a) < EPSILON) {
+      if (Math.abs(b) < EPSILON) {
+        return [];
+      }
+
+      const t = -c / (2 * b);
+      const xs = [Intersection(t, this)];
+
+      return this.intersectCaps(ray, xs);
+    }
+
+    const disc = b * b - 4 * a * c;
+
+    let t0 = (-b - Math.sqrt(disc)) / (2 * a);
+    let t1 = (-b + Math.sqrt(disc)) / (2 * a);
+
+    if (t0 > t1) {
+      const temp = t0;
+      t0 = t1;
+      t1 = temp;
+    }
+
+    let xs = [t0, t1].reduce((acc, curr) => {
+      const y = ray.origin.y + curr * ray.direction.y;
+
+      if (this.minimum < y && y < this.maximum) {
+        acc.push(Intersection(curr, this));
+      }
+      return acc;
+    }, []);
+
+    return this.intersectCaps(ray, xs);
+  }
+
+  localNormalAt(p) {
+    const dist = p.x * p.x + p.z * p.z;
+
+    if (dist < 1 && p.y >= this.maximum - EPSILON) {
+      return vector(0, 1, 0);
+    } else if (dist < 1 && p.y <= this.minimum + EPSILON) {
+      return vector(0, -1, 0);
+    }
+
+    let y = Math.sqrt(p.x * p.x + p.z * p.z);
+
+    if (p.y > 0) {
+      y = -y;
+    }
+
+    return vector(p.x, y, p.z);
+  }
+
+  /**
+   * @modifies xs
+   */
+  intersectCaps(ray, xs) {
+    if (!this.closed || Math.abs(ray.direction.y) < EPSILON) {
+      return xs;
+    }
+
+    // check for intersection with lower cap
+    let t = (this.minimum - ray.origin.y) / ray.direction.y;
+
+    if (checkCap(ray, t, this.minimum)) {
+      xs.push(Intersection(t, this));
+    }
+
+    // check for intersection with upper cap
+    t = (this.maximum - ray.origin.y) / ray.direction.y;
+
+    if (checkCap(ray, t, this.maximum)) {
+      xs.push(Intersection(t, this));
+    }
+
+    return xs;
+  }
+}
+
+export { Cone };
---
+++ src/lib/cone.test.js
@@ -0,0 +1,124 @@
+import { Cone } from "./cone";
+import { point, vector, normalize } from "./tuple";
+import { Ray } from "./ray";
+import { floatIsEqual } from "./math";
+
+describe("Cone", () => {
+  let shape;
+
+  beforeEach(() => {
+    shape = new Cone();
+  });
+
+  it("should be open by default", () => {
+    expect(shape.closed).not.toBeTruthy();
+  });
+
+  describe("ray intersection", () => {
+    xit("should detect non-intersection", () => {
+      const testCases = [
+        { origin: point(1, 0, 0), direction: vector(0, 1, 0) },
+        { origin: point(0, 0, 0), direction: vector(0, 1, 0) },
+        { origin: point(0, 0, -5), direction: vector(1, 1, 1) }
+      ];
+      testCases.forEach(t => {
+        const dir = normalize(t.direction);
+        const ray = Ray(t.origin, dir);
+        const xs = shape.localIntersect(ray);
+        expect(xs.length).toBe(0);
+      });
+    });
+
+    it("should detect intersections", () => {
+      const testCases = [
+        { origin: point(0, 0, -5), direction: vector(0, 0, 1), t0: 5, t1: 5 },
+        {
+          origin: point(0, 0, -5),
+          direction: vector(1, 1, 1),
+          t0: 8.66025,
+          t1: 8.66025
+        },
+        {
+          origin: point(1, 1, -5),
+          direction: vector(-0.5, -1, 1),
+          t0: 4.55006,
+          t1: 49.44994
+        }
+      ];
+      testCases.forEach(t => {
+        const ray = Ray(t.origin, normalize(t.direction));
+        const xs = shape.localIntersect(ray);
+        expect(xs.length).toBe(2);
+        expect(floatIsEqual(xs[0].t, t.t0)).toBeTruthy();
+        expect(floatIsEqual(xs[1].t, t.t1)).toBeTruthy();
+      });
+    });
+
+    it("should detect intersections with ray parallel to one of its halves", () => {
+      const ray = Ray(point(0, 0, -1), normalize(vector(0, 1, 1)));
+      const xs = shape.localIntersect(ray);
+      expect(xs.length).toBe(1);
+      expect(floatIsEqual(xs[0].t, 0.35355)).toBeTruthy();
+    });
+
+    describe("on constrained cylinder", () => {
+      beforeEach(() => {
+        shape.minimum = -0.5;
+        shape.maximum = 0.5;
+      });
+
+      it("should detect intersection with caps of closed cylinder", () => {
+        shape.closed = true;
+
+        const testCases = [
+          { origin: point(0, 0, -5), direction: vector(0, 1, 0), count: 0 },
+          { origin: point(0, 0, -0.25), direction: vector(0, 1, 1), count: 2 },
+          { origin: point(0, 0, -0.25), direction: vector(0, 1, 0), count: 4 }
+        ];
+        testCases.forEach(t => {
+          const ray = Ray(t.origin, normalize(t.direction));
+          const xs = shape.localIntersect(ray);
+          expect(xs.length).toBe(t.count);
+        });
+      });
+    });
+  });
+
+  describe("normal", () => {
+    it("should be computed for points on surface", () => {
+      const testCases = [
+        { point: point(0, 0, 0), normal: vector(0, 0, 0) },
+        { point: point(1, 1, 1), normal: vector(1, -Math.sqrt(2), 1) },
+        { point: point(-1, -1, 0), normal: vector(-1, 1, 0) }
+      ];
+      testCases.forEach(t => {
+        expect(shape.localNormalAt(t.point)).toEqualVector(t.normal);
+      });
+    });
+
+    it("should be computed for points on closed cyclinder caps", () => {
+      shape.closed = true;
+      shape.minimum = 1;
+      shape.maximum = 2;
+
+      const testCases = [
+        { point: point(0, 1, 0), normal: vector(0, -1, 0) },
+        { point: point(0.5, 1, 0), normal: vector(0, -1, 0) },
+        { point: point(0, 1, 0.5), normal: vector(0, -1, 0) },
+        { point: point(0, 2, 0), normal: vector(0, 1, 0) },
+        { point: point(0.5, 2, 0), normal: vector(0, 1, 0) },
+        { point: point(0, 2, 0.5), normal: vector(0, 1, 0) }
+      ];
+      testCases.forEach(t => {
+        expect(shape.localNormalAt(t.point)).toEqualVector(t.normal);
+      });
+    });
+  });
+
+  describe("bounds", () => {
+    it("should have infinite minimum and maximum bounds", () => {
+      expect(shape.minimum).toBe(Number.NEGATIVE_INFINITY);
+      expect(shape.maximum).toBe(Number.POSITIVE_INFINITY);
+    });
+  });
+});
