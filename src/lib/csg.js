48db128c8ddfdc74af4f3683471ce31c253a8fbd
Marc Mauger <simianarmy@gmail.com>
2019-10-19 18:52:58 -0700

    stuck at failing test

--- src/lib/csg.js
+++ src/lib/csg.js
@@ -1,8 +1,27 @@
 import { Shape } from "./shape";
+import { Group } from "./group";
 
 export const makeCsg = (operation, left, right) => {
   return new Csg({operation, left, right});
 };
+
+/**
+ * @param {Shape} a
+ * @param {Shape} b
+ * @returns {Boolean} iff shape A includes shape B
+ */
+function objectIncludes(a, b) {
+  if (a instanceof Group) {
+    return a.shapes.any(shape => {
+      return objectIncludes(shape, b);
+    });
+  } else if (a instanceof Csg) {
+    return objectIncludes(a.left, b) ||
+      objectIncludes(a.right, b);
+  } else {
+    return a == b;
+  }
+}
 
 class Csg extends Shape {
   static Union = "union";
@@ -29,6 +48,44 @@ class Csg extends Shape {
     this.right = props.right;
     this.left.parent = this;
     this.right.parent = this;
+  }
+
+  /**
+   * @param {Array[Intersection]}
+   * @returns {Array[Intersection]}
+   */
+  filterIntersections(xs) {
+    let inl = false;
+    let inr = false;
+
+    return xs.filter(is => {
+      let isAllowed = false;
+      // If is.object is part of the "left" child
+      const lhit = objectIncludes(this.left, is.object);
+
+      if (Csg.intersectionAllowed(this.operation, lhit, inl, inr)) {
+        isAllowed = true;
+      }
+
+      // depending on which object was hit, toggle either inl or inr
+      if (lhit) {
+        inl = !inl;
+      } else {
+        inr = !inr;
+      }
+
+      return isAllowed;
+    });
+  }
+
+  localIntersect(ray) {
+    const leftXs = this.left.localIntersect(ray);
+    console.log("left xs", leftXs);
+    const rightXs = this.right.localIntersect(ray);
+    console.log("right xs", rightXs);
+    const xs = leftXs.concat(rightXs).sort((a, b) => a.t < b.t ? -1 : a.t > b.t ? 1 : 0);
+
+    return this.filterIntersections(xs);
   }
 }
 
--- src/lib/csg.test.js
+++ src/lib/csg.test.js
@@ -1,6 +1,10 @@
 import { Csg, makeCsg } from "./csg";
 import { Sphere } from "./sphere";
 import { Cube } from "./cube";
+import { Intersection } from "./intersection";
+import { Ray } from "./ray";
+import { point, vector } from "./tuple";
+import { translation } from "./transformations";
 
 describe("Csg", () => {
   it("should create object from operation and 2 shapes", () => {
@@ -48,5 +52,46 @@ describe("Csg", () => {
         expect(Csg.intersectionAllowed(op[0], op[1], op[2], op[3])).toBe(op[4]);
       });
     });
+  });
+
+  it("should filter list intersections", () => {
+    const s1 = new Sphere();
+    const s2 = new Cube();
+    const xs = [Intersection(1, s1), Intersection(2, s2), Intersection(3, s1), Intersection(4, s2)];
+
+    const opTable = [
+      [Csg.Union, 0, 3],
+      [Csg.Intersection, 1, 2],
+      [Csg.Difference, 0, 1]
+    ];
+    opTable.forEach(op => {
+      const csg = makeCsg(op[0], s1, s2);
+      const res = csg.filterIntersections(xs);
+      expect(res.length).toBe(2);
+      expect(res[0]).toEqual(xs[op[1]]);
+      expect(res[1]).toEqual(xs[op[2]]);
+    });
+  });
+
+  it("ray miss should detect no intersections", () => {
+    const csg = makeCsg(Csg.Union, new Sphere(), new Cube());
+    const r = Ray(point(0, 2, -5), vector(0, 0, 1));
+    const xs = csg.localIntersect(r);
+    expect(xs.length).toBe(0);
+  });
+
+  it("ray hit should find all intersections", () => {
+    const s1 = new Sphere();
+    const s2 = new Sphere();
+    s2.setTransform(translation(0, 0, 0.5));
+    const csg = makeCsg(Csg.Union, s1, s2);
+    const r = Ray(point(0, 0, -5), vector(0, 0, 1));
+    const xs = csg.localIntersect(r);
+    console.log(xs);
+    expect(xs.length).toBe(2);
+    expect(xs[0].t).toBe(4);
+    expect(xs[0].object).toEqual(s1);
+    expect(xs[1].t).toBe(6.5);
+    expect(xs[1].object).toEqual(s2);
   });
 });
